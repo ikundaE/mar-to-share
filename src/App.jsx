2003b63ef8e4f102c1756104b5d0dfab46a0aed2
ikundaE <enoch.ikunda@gmail.com>
2023-11-18 19:39:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { MedicationRecord } from '../components/MedicationRecord'
+
+export default function App() {
+
+  return (
+    <>
+      <MedicationRecord />
+    </>
+  )
+}
